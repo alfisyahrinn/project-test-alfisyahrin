590606384364f922c45d69a8e67ba855095f773d
Alfi Syahrin <sahrin201@gmail.com>
2023-12-24 02:01:15 +0700

    feat(careeres) add page and route

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Ini adalah halaman Contact</h1>
+      <h1>BLOG</h1>
+      <div>
+        {/* {data.map((datas: data, index) => (
+          <div key={index} className="p-3 border rounded-lg mb-3">
+            <h3> {datas.name} </h3>
+            <p>{datas.price}</p>
+            <small>{datas.size}</small>
+          </div>
+        ))} */}
+      </div>
+    </div>
+  );
+}
