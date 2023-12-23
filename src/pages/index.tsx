61b00fadb164e55b5fec0ddd74a5d0254f2aa26c
Alfi Syahrin <sahrin201@gmail.com>
2023-12-24 04:31:37 +0700

    fix(work) error in build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,6 @@ export default function Home() {
       <div className="flex flex-col gap-4 h-[80vh] justify-center items-center text-center">
         <h1 className="text-lg font-bold">Service</h1>
         <p className="">Silakan ke halaman ideas</p>
-        <Link href={'/ideas'}>
-          <Button variant="secondary">Lets' Go</Button>
-        </Link>
       </div>
     </div>
   );
